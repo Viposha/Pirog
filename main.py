4c9b315bffaff36f4f6163a365ec3509cdce39fc
Ivan <ivan.pyrog@halliburton.com>
2022-04-10 14:17:19 +0300

    Added my comments to the main file

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import sys
+import sys # TODO now import only the function that is being used from module
 import time
 
 start = time.time()
+
 set = range(100000)
 print(f' The length of set is {len(set)} numbers')
 
 list = list(set)
 tuple = tuple(set)
+# TODO: call list and tuple funcs several times
 
 print(type(list))
 print(type(tuple))
 
 print(f'Size of list is {sys.getsizeof(list)} bytes')
 print(f'Size of tuple is {sys.getsizeof(tuple)} bytes')
+# TODO reformat code to fit only 89 positions per line
 print(f'The difference in memory between tuple and list is {sys.getsizeof(list)-sys.getsizeof(tuple)} bytes')
 
 end = time.time()
